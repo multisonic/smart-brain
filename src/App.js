5b46d02cd341bd6dee1dbb2f62cb2aa9f69455cf
Michael Zaleski <me@mikezaleski.com>
2020-02-28 11:39:30 -0500

    working on ImageRecAPI video

--- src/App.js
+++ src/App.js
@@ -1,35 +1,71 @@
 import React, { Component } from "react";
+import Particles from "react-particles-js";
+import Clarifai from 'clarifai';
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
-import Particles from 'react-particles-js';
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+
+const app = new Clarifai.App({
+  apiKey: '99d13529322f46f5b711574ea9034335'
+ });
 
 const particlesOptions = {
-  "particles": {
-    "number": {
-      "value": 175,
-      "density": {
-        "enable": true,
-        "value_area": 800
+  particles: {
+    number: {
+      value: 175,
+      density: {
+        enable: true,
+        value_area: 800
       }
     }
-  } 
-}
+  }
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  };
+
+  onButtonSubmit = () => {
+    this.setState({imageURL: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(
+      function(response) {
+        console.log(response);
+        console.log('input url:', this.state.input)
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  };
+
   render() {
     return (
       <div className="App">
-        <Particles className ='particles' 
-          params={particlesOptions} 
-        />
+        <Particles className="particles" params={particlesOptions} />
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-       {/* <FaceRecognition/> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -9,7 +9,7 @@ const Logo = () => {
       <Tilt
         className="Tilt br2 shadow-2"
         options={{ max: 55 }}
-        style={{ height: 250, width: 250 }}
+        style={{ height: 100, width: 100 }}
       >
         <div className="Tilt-inner pa3"> <img style={{paddingTop: '5px'}} src={ brain } alt="Brain"/> </div>
       </Tilt>
