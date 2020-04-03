58951fa83b6c5591051dac17452cd65061af0b7b
Michael Zaleski <me@mikezaleski.com>
2020-04-03 07:09:46 -0400

    fix typo in /image endpoint url

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("https://aqueous-springs-20184.herokuapp.com//image", {
+          fetch("https://aqueous-springs-20184.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
